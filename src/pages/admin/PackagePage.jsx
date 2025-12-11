fa3e5fd6c678d24a0d4b9affdc5dce3ac6261a6e
b3rtuso <angelbertgonzales920@gmail.com>
2025-12-11 11:44:19 +0800

    Day 10 - Create button for adding tour packages

--- src/pages/admin/PackagePage.jsx
+++ src/pages/admin/PackagePage.jsx
@@ -72,6 +72,11 @@ export default function PackagePage() {
           </p>
         )}
       </div>
+      <div className="flex justify-end gap-3 mt-6">
+        <button className="px-6 py-2 rounded-full bg-[#d3ebd7] text-sm font-medium hover:bg-[#c1dfc7]">
+          + Add Tour
+        </button>
+      </div>
     </div>
   );
 }
